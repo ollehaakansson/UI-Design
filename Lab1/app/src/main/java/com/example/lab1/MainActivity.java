99957a9de9230e523107439c1bbdd11f8e34b7e2
Eleanor Brunskog <eleanor@Eleanors-MacBook-Pro.local>
2023-11-15 15:31:41 +0100

    Update lab1

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -12,8 +11,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,8 +27,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText input;
     private int currChild = -1;
     private int currParent = -1;
+    private int parentCounter = 0;
+    private int[] parentCounter1 = {0,0,0};
     private int lastChild;
     private int lastParent;
+    private boolean hasClicked;
+    private boolean hasTyped;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,18 +47,66 @@ public class MainActivity extends AppCompatActivity {
         expandableListAdapter = new MyExpandableListAdapter(this, groupList, mobileCollection);
         expandableListView.setAdapter(expandableListAdapter);
 
-        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                String selected = expandableListAdapter.getChild(groupPosition, childPosition).toString();
-                Toast.makeText(getApplicationContext(), "Selected " + selected, Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-
+        expandableListView.setOnGroupClickListener(getGroupListener());
+        expandableListView.setOnChildClickListener(getClickListener());
         input.addTextChangedListener(getTextWatcher()); //listener
 
     }
+
+    private ExpandableListView.OnGroupClickListener getGroupListener() {
+        return new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+
+                hasClicked = true;
+                parentCounter++;
+                lastParent = currParent;
+                currParent = groupPosition;
+                parentCounter1[groupPosition] ++;
+                Log.d("MESSAGE", String.valueOf(parentCounter1[groupPosition]));
+
+                for(int i = 0; i < parentCounter1.length; i++){
+                    if(parentCounter1[i] >= 2){
+                        Log.d("MESSAGE", "Jag körs");
+                        collaplseChosenGroup(groupPosition);
+                        Log.d("MESSAGE", "Jag körs 2");
+                        i = 0;
+                        break;
+                    }
+                }
+
+               if(lastParent != currParent){
+                    expandableListView.collapseGroup(lastParent);
+                    parentCounter = 0;
+                }
+
+                input.setText("/" + expandableListAdapter.getGroup(groupPosition).toString() + "/");
+                deColorInput();
+
+                return true;
+            }
+        };
+    }
+
+    private void collaplseChosenGroup(int groupPosition) {
+        expandableListView.collapseGroup(groupPosition);
+    }
+
+    private ExpandableListView.OnChildClickListener getClickListener() {
+        return new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                hasClicked = true;
+                colorChildGreen(groupPosition, childPosition);
+                deColorInput();
+                input.setText("/" + expandableListAdapter.getGroup(groupPosition).toString() + "/" + expandableListAdapter.getChild(groupPosition, childPosition).toString() + "/");
+                
+
+                return true;
+            }
+        };
+    }
+
 
     public TextWatcher getTextWatcher() {
         return new TextWatcher() {
@@ -72,11 +121,12 @@ public class MainActivity extends AppCompatActivity {
                 String[] selectedPartsTemp = selected.split("/");
                 String[] selectedParts = Arrays.stream(selectedPartsTemp).filter(s -> !s.isEmpty()).toArray(String[]::new); // Denna tar ba bort alla null-objekt i arrayen, tydligen funkade ej replaceAll så de får bli såhär
                 Log.d("MESSAGE", selectedParts[0]);
-                if (selected.isEmpty()) { //Kollar om input är empty
+                if (selected.isEmpty()) {
                     input.setText("/");
                 } else { if(selected.endsWith("/") && selected.length() > 1){
                             for(int i = 0; i<groupList.size(); i++) {
                                 if (selectedParts[0].equals(groupList.get(i))) {
+                                    deColorInput();
                                     expandableListView.expandGroup(i);
                                     lastParent = currParent;
                                     currParent = i;
@@ -85,7 +135,9 @@ public class MainActivity extends AppCompatActivity {
                                     }if(selectedParts.length>1){
                                         for(int j = 0; j < mobileCollection.get(groupList.get(currParent)).size(); j++){
                                             Log.d("MESSAGE", mobileCollection.get(groupList.get(currParent)).get(j));
+                                            deColorInput();
                                             if(mobileCollection.get(groupList.get(currParent)).get(j).equals(selectedParts[1])) {
+                                                hasClicked = false;
                                                 colorChildGreen(currParent, j);
                                                 deColorInput();
                                                 lastChild = currChild;
@@ -93,14 +145,21 @@ public class MainActivity extends AppCompatActivity {
                                                 if(lastChild != -1) {
                                                     deColorChild(lastParent, lastChild);
                                                 }
-                                                }else{
+                                                }else if (!hasClicked && !mobileCollection.get(groupList.get(currParent)).get(j).equals(selectedParts[1])){
+                                                Log.d("MESSAGE", "hej jag körs");
                                                 colorInputRed();
                                             }
                                         }
                                     }
                     }else if(!selected.endsWith("/") && selectedParts.length<2){
                         for(int a = 0; a < groupList.size(); a++) { // Kollapsar alla grupper om ingen grupp ska expanderas
-                            expandableListView.collapseGroup(a);
+                            for(int b = 0; b < groupList.get(a).length(); b++){
+                                expandableListView.collapseGroup(a);
+                                deColorChild(a, b);
+                                deColorInput();
+                            }
+
+
                         }
                     }
                 }
@@ -116,13 +175,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void colorChildGreen(int parentID, int childID) {
+        for(int i = 0; i < groupList.get(parentID).length(); i++) {
+            deColorChild(parentID, i);
+        }
         int childIndex = expandableListView.getFlatListPosition(ExpandableListView.getPackedPositionForChild(parentID, childID));
         expandableListView.getChildAt(childIndex).setBackgroundColor(Color.GREEN);
+        deColorInput();
     }
 
     public void deColorChild(int parentID, int childID){
         int childIndex = expandableListView.getFlatListPosition(ExpandableListView.getPackedPositionForChild(parentID, childID));
         expandableListView.getChildAt(childIndex).setBackgroundColor(Color.WHITE);
+        deColorInput();
     }
 
 
